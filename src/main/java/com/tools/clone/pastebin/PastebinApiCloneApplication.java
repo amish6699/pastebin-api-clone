6b2a7364429200cdd1278ac551dca4367d63d574
Ashutosh Mishra <amish6699@gmail.com>
2022-10-22 13:27:52 +0530

    first commit

---
+++ src/main/java/com/tools/clone/pastebin/PastebinApiCloneApplication.java
@@ -0,0 +1,13 @@
+package com.tools.clone.pastebin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastebinApiCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastebinApiCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tools/clone/pastebin/PastebinApiCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tools.clone.pastebin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastebinApiCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
