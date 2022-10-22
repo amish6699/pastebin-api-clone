5a0a96694174913fc8b1fa228bbce74130de9b21
Ashutosh Mishra <amish6699@gmail.com>
2022-10-22 13:37:02 +0530

    created packages, ddl and yaml

--- src/main/java/com/tools/clone/pastebin/PasteBinApiCloneApplication.java
+++ src/main/java/com/tools/clone/pastebin/PasteBinApiCloneApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PastebinApiCloneApplication {
+public class PasteBinApiCloneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PastebinApiCloneApplication.class, args);
+		SpringApplication.run(PasteBinApiCloneApplication.class, args);
 	}
 
 }
